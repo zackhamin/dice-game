228b2336df380c821fab76d2a4122f92fc6d8858
Zack Amin <ishaqhamin@gmail.com>
2019-11-19 17:58:06 +0000

    logic finished

--- dice.js
+++ dice.js
@@ -7,16 +7,19 @@ let playerTwoScore = 0;
 let randomRoll;
 
 
-
 let diceRoll = () =>{
     randomRoll = Math.floor((Math.random() * 6) +1);
     console.log(randomRoll)
 }
-
-
+const gameStart = () =>{
+    console.log("lets play!")
+    diceRoll()
+}
+gameStart();
 
 const playerTurn = () => {
     diceRoll()
+    playerLose();
     
         console.log(playerOneRoll)
     if (playerOneRoll) {
@@ -28,10 +31,20 @@ const playerTurn = () => {
     else {
         playerTwoScore += randomRoll
         playerOneRoll = true
-        console.log(playerOneRoll)
+        console.log(`The roll was: ${randomRoll}`)
         console.log(`Player two score: ${playerTwoScore}`)
     }
- 
+}
+
+const playerLose = () =>{
+    if (randomRoll === 1){
+        console.log("You rolled 1, you lose!")
+        console.log("------------")
+        playerOneScore = 0;
+        playerTwoScore = 0;
+        console.log(playerOneScore);
+        gameStart();
+    }
 }
 
 playerTurn();
